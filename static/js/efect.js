9bd1aeee49e79e6514f3d1f1b08439713ae830b3
leiqne <joaquincastillaeguia7@gmail.com>
2024-05-20 16:06:05 -0500

    se hizo la seccion de comentarios con carrucel to-do pues las otras 5 secciones y hacerlo responsive

--- static/js/efect.js
+++ static/js/efect.js
@@ -22,7 +22,6 @@ function fadeOut(element) {
         }
     })();
 }
-
 function toggleIntegrantes() {
     var container = document.getElementsByClassName("integrantes")[0];
     var toggleButton = document.getElementById("toggleButton");
@@ -35,3 +34,36 @@ function toggleIntegrantes() {
         toggleButton.textContent = "Ver más";
     }
 }
+document.addEventListener('DOMContentLoaded', (event) => {
+    let slider = document.querySelector('.slider-contenedor');
+    let sliderItems = document.querySelectorAll('.slider-test');
+    let contador = 1;
+    let tamW = sliderItems[0].clientWidth;
+    let intervalo = 5000;
+
+    // Duplica los elementos para un deslizamiento continuo
+    let sliderLength = sliderItems.length;
+    for (let i = 0; i < sliderLength; i++) {
+        let clone = sliderItems[i].cloneNode(true);
+        slider.appendChild(clone);
+    }
+
+    function slides() {
+        slider.style.transform = `translateX(${-tamW * contador}px)`;
+        slider.style.transition = 'transform 0.5s ease-in-out';
+
+        contador++;
+
+        if (contador === sliderItems.length + 1) {
+            setTimeout(() => {
+                slider.style.transition = 'none';
+                slider.style.transform = `translateX(0px)`;
+                contador = 1;
+            }, 500); // Ajusta el tiempo para que coincida con la duración de la transición
+        }
+    }
+
+    setInterval(slides, intervalo);
+});
+
+
