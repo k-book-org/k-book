dc065ea444b86fae5409e9502c7153726a6dc8d1
nakato156 <christianvelasces@gmail.com>
2024-06-04 20:07:33 -0500

    feat: formulario login

--- k-book-app/src/app/app-routing.module.ts
+++ k-book-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'home', component: LandingComponent }
+  { path: 'home', component: LandingComponent },
+  { path: 'login', component:LoginComponent }
 ];
 
 @NgModule({
