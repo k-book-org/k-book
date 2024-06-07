01f5c6956451656725dd05ee92484daf6a86c840
Joaquin Eguia <joaquincastillaeguia7@gmail.com>
2024-06-07 14:29:55 -0500

    fix/bugs-merge

--- k-book-app/src/app/app-routing.module.ts
+++ k-book-app/src/app/app-routing.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
-import { PerfilComponent } from './perfil/perfil.component';
-
-
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule) },
-
-  { path: 'login', component:LoginComponent },
-  { path: 'registro', component:RegistroComponent },
-  { path: 'perfil', component:PerfilComponent }
 
   { path: 'comunidad', loadChildren: () => import('./core/comunidad/comunidad.module').then(m => m.ComunidadModule) },
 
--- k-book-app/src/app/app.module.ts
+++ k-book-app/src/app/app.module.ts
@@ -5,20 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
-import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
-import { SharedModule } from './shared/shared.module';
-import { PerfilComponent } from './perfil/perfil.component'; 
-
-
 @NgModule({
   declarations: [
     AppComponent,
-
-    LoginComponent,
-    RegistroComponent,
-    PerfilComponent,
-
   ],
   imports: [
     BrowserModule,
