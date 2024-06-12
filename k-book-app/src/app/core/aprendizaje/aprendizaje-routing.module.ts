8bdc9d37929d2b72c9717b7ee1cbfacaaa69927b
Brunix3017 <brunomedinaagnini@gmail.com>
2024-06-12 11:31:34 -0500

    Enrutamiento de la US espacio de trabajo

--- k-book-app/src/app/core/aprendizaje/aprendizaje-routing.module.ts
+++ k-book-app/src/app/core/aprendizaje/aprendizaje-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AprendizajeComponent } from './aprendizaje.component';
+import { EspacioDeTrabajoComponent } from './espacio-de-trabajo/espacio-de-trabajo.component';
 
-const routes: Routes = [{ path: '', component: AprendizajeComponent }];
+const routes: Routes = [
+  { path: '', component: AprendizajeComponent },
+  { path: 'espacio-de-trabajo', component: EspacioDeTrabajoComponent}
+]; 
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
